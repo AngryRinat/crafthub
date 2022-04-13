ec1be2902105561e1e24ce2294cdbd58bc3485ee
angrykeo <angrykeo@gmail.com>
2022-04-14 00:18:16 +0300

    Added bugs

--- products/views.py
+++ products/views.py
@@ -1,32 +1,27 @@
-from django.shortcuts import render
+from products.utils import DataMixin
 from django.views.generic import ListView, DetailView
 from products.models import Product, Category
 
-menu = [{'title': "О сайте", 'url_name': 'about'},
-        {'title': "Добавить статью", 'url_name': 'add_page'},
-        {'title': "Обратная связь", 'url_name': 'contact'},
-        {'title': "Войти", 'url_name': 'login'}
-]
 
 
 
-class ProductListView(ListView):
+
+class ProductListView(DataMixin, ListView):
+    paginate_by = 2
+
     model = Product
     template_name = 'products/index.html'
 
-    def __str__(self):
-        return self.name
-
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = dict()
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Главая страница')
         context['productlist'] = Product.objects.all()
         context['categorylist'] = Category.objects.all()
-        context['title'] = 'Заглавная страница'
-        context['menu'] = menu
-        return context
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
-        return Product.objects.filter()
+        return Product.objects.all()
+
 
 
 class ProductDetailView(DetailView):
@@ -37,18 +32,24 @@ class ProductDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = context['product']
-        context['menu'] = menu
+
+        def get_context_data(self, *, object_list=None, **kwargs):
+            context = super().get_context_data(**kwargs)
+            c_def = self.get_user_context(title=context['title'])
+            context['productlist'] = Product.objects.all()
+            context['categorylist'] = Category.objects.all()
+            return dict(list(context.items()) + list(c_def.items()))
+
         return context
+
 
 class ProductCategory(ListView):
     model = Product
     template_name = 'products/index.html'
     context_object_name = 'productlist'
 
-
     def get_queryset(self):
-         return Product.objects.filter(cat__slug=self.kwargs['cat_slug'])
+        return Product.objects.filter(cat__slug=self.kwargs['cat_slug'])
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)

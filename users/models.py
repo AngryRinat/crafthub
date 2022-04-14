5124969ea734fa53ac3445062c14757b39d56a12
RinoReinz <angrykeo@gmail.com>
2022-04-14 18:02:13 +0300

    update 23 march

--- users/models.py
+++ users/models.py
@@ -3,3 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True, null=True)
+
+
+    
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 
-# Create your views here.
+from products.utils import DataMixin
+from django.views.generic import CreateView
+from users.forms import UserRegistrationForm
+from users.models import User
+
+class RegisterUser(DataMixin, SuccessMessageMixin, CreateView):
+    form_class = UserRegistrationForm
+    model = User
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('products:index')
+    success_message = 'Success!'

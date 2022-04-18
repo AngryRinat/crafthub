60b6e7183cd17a06ac539b24b04937c66a47c38e
Rinat <angrykeo@gmail.com>
2022-04-18 21:34:20 +0300

    fixed some bugs

--- baskets/views.py
+++ baskets/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import ListView
@@ -15,11 +16,12 @@ def basketapp(request):
     }
     return render(request, 'baskets/baskets.html', context)
 
+@login_required
 def basket_add(request, product_id):
-    product = Product.object.get(id=product_id)
-    baskets = Basket.object.filter(user = request.user, product=product)
-    if not baskets.exists:
-            baskets = Basket.object.create(user=request.user, product=product, qty=1)
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user = request.user, product=product)
+    if not baskets.exists():
+            baskets = Basket.objects.create(user=request.user, product=product, qty=1)
             return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
     else:
@@ -27,3 +29,9 @@ def basket_add(request, product_id):
             basket.qty += 1
             basket.save()
             return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def basket_remove(request, id):
+    Basket.objects.get(id=id).delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))

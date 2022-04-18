b8c85002998d0a5e08704cc2379f4d441ffb7f1f
RinoReinz <angrykeo@gmail.com>
2022-04-18 18:31:37 +0300

    fixed bugs with basket

--- baskets/urls.py
+++ baskets/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from baskets.views import basketapp
+from baskets.views import basketapp, basket_add
 
 app_name = 'baskets'
 
 urlpatterns = [
     path('', basketapp, name='baskets'),
+    path('basket_add/<int:product_id>', basket_add, name='basket_add'),
 ]
--- baskets/views.py
+++ baskets/views.py
@@ -1,8 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import ListView
 from baskets.models import Basket
-from users.models import User
-
+from products.models import Product
 
 
 
@@ -14,3 +14,16 @@ def basketapp(request):
         'title': title
     }
     return render(request, 'baskets/baskets.html', context)
+
+def basket_add(request, product_id):
+    product = Product.object.get(id=product_id)
+    baskets = Basket.object.filter(user = request.user, product=product)
+    if not baskets.exists:
+            baskets = Basket.object.create(user=request.user, product=product, qty=1)
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+    else:
+            basket = baskets.first()
+            basket.qty += 1
+            basket.save()
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))

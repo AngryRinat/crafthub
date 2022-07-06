08ffdfcc66b873e65ba1e0a78367f47118c5071b
angrykeo <angrykeo@gmail.com>
2022-07-06 23:22:10 +0300

    Added safe delete

--- admins/urls.py
+++ admins/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', AdminsIndexView.as_view(), name='admins_index'),
     path('users_read', AdminsUserView.as_view(), name='users_read'),
     path('users_create', admins_create_user, name='users_create'),
-    path('users_delete/<int:pk>/', UserDeleteView.as_view(), name='users_delete'),
+    path('users_delete/<int:pk>', admin_user_delete, name='users_delete'),
 ]
--- admins/views.py
+++ admins/views.py
@@ -33,12 +33,9 @@ def admins_create_user(request):
     return render(request, 'admins/users_create.html', context)
 
 
-class UserDeleteView(UpdateView):
-    model = User
-    template_name = 'admins/users_read.html'
-    success_url = reverse_lazy('products:index')
+def admin_user_delete(request,pk):
+    user = User.objects.get(id=pk)
+    user.is_active = False
+    user.save()
+    return HttpResponseRedirect(reverse('admins:admins_index'))
 
-    def delete(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        self.object.safe_delete()
-        return HttpResponseRedirect(self.success_url)
